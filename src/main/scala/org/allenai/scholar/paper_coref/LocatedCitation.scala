39e7b225297fa2b4e98d157f479b1725a59e3407
Nicholas Monath <nicholas.monath@gmail.com>
2015-06-06 20:05:41 -0400

    JSON Serialization

--- src/main/scala/org/allenai/scholar/paper_coref/LocatedCitation.scala
+++ src/main/scala/org/allenai/scholar/paper_coref/LocatedCitation.scala
@@ -5,6 +5,31 @@ import cc.factorie._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+// TODO: This has issues with EmptyValueStrategy
+trait JSONSerializable {
+  def toJSON: String
+}
+
+object JSONSerializable {
+
+  def writeJSON(citations: Iterator[JSONSerializable], file: File, codec: String = "UTF-8", bufferSize: Int = 10) = {
+    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), codec))
+    val citationStrings = citations.map(_.toJSON)
+    if (citationStrings.hasNext)
+      writer.write(citationStrings.next())
+    if (citationStrings.hasNext)
+      citationStrings.grouped(bufferSize).foreach {
+        group =>
+          group.foreach {
+            citation =>
+              writer.newLine()
+              writer.write(citation)
+          }
+          writer.flush()
+      }
+  }
+}
+
 object ParsedPaper {
   def fromCitations(cits:Iterable[LocatedCitation]):ParsedPaper = {
     assert(cits.count(_.paperId.isDefined) == 1)
@@ -12,8 +37,9 @@ object ParsedPaper {
   }
 }
 
-case class ParsedPaper(self:LocatedCitation, bib:Iterable[LocatedCitation]) {
+case class ParsedPaper(self:LocatedCitation, bib:Iterable[LocatedCitation]) extends JSONSerializable{
   override def toString = s"(ParsedPaper(${self.paperId.getOrElse("UnknownId")}), self: ${self.toString}, bib: ${bib.map(_.toString).mkString(", ")})"
+  def toJSON: String = compact(render(Extraction.decompose(this)(DefaultFormats)))
 }
 
 case class RawCitation(rawTitle:String, rawAuthors:List[String], date:String) {
@@ -21,10 +47,11 @@ case class RawCitation(rawTitle:String, rawAuthors:List[String], date:String) {
   def isEmpty: Boolean = rawTitle.isEmpty && rawAuthors.isEmpty && date.isEmpty 
 }
 
-case class LocatedCitation(rawCitation:RawCitation, citingPaperId:Option[String], paperId:Option[String]) {
+case class LocatedCitation(rawCitation:RawCitation, citingPaperId:Option[String], paperId:Option[String]) extends JSONSerializable {
   lazy val foundInId = citingPaperId.getOrElse(paperId.get)
   override def toString = s"(LocatedCitation, rawCitation:${rawCitation.toString}, citingPaperId: $citingPaperId, paperId: $paperId)"
   def isEmpty: Boolean = rawCitation.isEmpty
+  def toJSON: String = compact(render(Extraction.decompose(this)(DefaultFormats)))
 }
 
 object LocatedCitation {

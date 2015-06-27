c90b6fd480ee102698004bb5d51baffb486d6d0e
Nicholas Monath <nicholas.monath@gmail.com>
2015-06-26 23:54:43 -0400

    added documentation

--- src/main/scala/org/allenai/scholar/paper_coref/coreference/AlphaOnly.scala
+++ src/main/scala/org/allenai/scholar/paper_coref/coreference/AlphaOnly.scala
@@ -1,8 +1,16 @@
 package org.allenai.scholar.paper_coref.coreference
 
+/**
+ * A very simple coreference scheme which considers two papers coreferent if the titles are a case insensitive match. 
+ */
 object AlphaOnly extends HashingCoref {
 
   override val name = "AlphaOnly"
 
+  /**
+   * This implementation of titleHash lowercases the title and normalizes white spaces. *
+   * @param title - the title of the paper as it appeared in the extraction
+   * @return - the relaxed version of the title.
+   */
   def titleHash(title: String) = title.replaceAll("""\s+""", " ").toLowerCase.replaceAll("""^[\w]""","")
 }
--- src/main/scala/org/allenai/scholar/paper_coref/coreference/Baseline.scala
+++ src/main/scala/org/allenai/scholar/paper_coref/coreference/Baseline.scala
@@ -1,50 +1,27 @@
 package org.allenai.scholar.paper_coref.coreference
 
-import java.io.File
-
 import cc.factorie.app.nlp.Document
 import cc.factorie.app.nlp.segment.DeterministicTokenizer
 import cc.factorie.app.strings
-import cc.factorie.util.EvaluatableClustering
-import org.allenai.scholar.paper_coref._
-import org.allenai.scholar.paper_coref.data_structures._
 
-
+/**
+ * A relaxed string match coreference algorithm. The algorithm considers two papers as coreferent
+ * if the lower-cased, stemmed version of the paper titles excluding punctuation are equal. 
+ */
 object Baseline extends HashingCoref {
 
   override val name = "Baseline"
 
   val fieldSep = Character.toString(31.toChar)
+
+  /**
+   * This implementation of titleHash removes punctuation, stems and lowercases the title
+   * @param rawTitle - the title as extracted from paper
+   * @return - the relaxed version of the title string.
+   */
   def titleHash(rawTitle:String):String =
     DeterministicTokenizer.process(new Document(rawTitle))
       .tokens.filterNot(_.isPunctuation).map(t => strings.porterStem(t.string.toLowerCase)).mkString(fieldSep)
 
-
-  //def performCoref(mentions: Iterable[PaperMention]) = mentions.groupBy(m => if(m.title.nonEmpty) corefTitleHash(m.title) else m.id).values
-
-  def main(args:Array[String]) {
-
-    val citsDir = args(0)
-    val locCits = new File(citsDir).listFiles().flatMap(f => LocatedCitation.fromFile(f.getAbsolutePath)).toIterable
-
-    val goldCitDocs = {
-      val pms = PaperMetadataWithId.fromFile(args(1))
-      val bcs = BareCitation.fromFile(args(2))
-      val pmMap = pms.map(p => p.id -> p).toMap
-      bcs.groupBy(_.from).flatMap{ case (k, vs) =>
-        pmMap.get(k).map(pm => GoldCitationDoc(pm, vs.flatMap(v => pmMap.get(v.to))))
-      }.map(g => g.doc.id -> g).toMap
-    }
-    val citsMap = locCits.groupBy(_.foundInId).mapValues(ParsedPaper.fromCitations)
-
-
-    val paperMentions = PaperMention.generate(citsMap, goldCitDocs)//.filterNot(_.title.trim.isEmpty)
-
-    val prediction = performCoref(paperMentions)
-
-    val gold = prediction.trueClustering
-    val pred = prediction.predictedClustering
-
-    println(EvaluatableClustering.evaluationString(pred, gold))
-  }
+  
 }
